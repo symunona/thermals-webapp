95a123fcc5eb11d213cfe7f296b0bb31846a9c6c
symunona <symunona@gmail.com>
2024-06-22 15:45:38 +0200

    about up and added tracker.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -26,26 +26,31 @@ const AboutModal = () => {
       <div class="modal">
         <div class="modal-content">
             <div>
-            <h2>thermals.tmpx.space DEMO</h2>
+            <h2>thermal.tmpx.space DEMO</h2>
             <p>
-                When I started flying, <a>thermal.kk7.ch</a> was
-                an awesome resource as a rookie pilot. As I gained some
-                experience and went off the map, I realized that the
-                skyways are especially useful for planning XC flights but
-                it would be even more useful to have the individual flights
-                navigable in 3D to understand how a certain region works.
+                It's like <a target="_blank" href="https://thermal.kk7.ch">thermal.kk7.ch</a>,
+                except it's 3D!
             </p>
             <p>
-                This is my contribution to the flying community. I hope
-                with this, you'll be able to understand how to navigate
-                landscapes all around the world safely.
+                A map which shows where the thermals were before based on flights already done.
+            </p>
+            <p>
+                I made this to understand how to fly cross country and navigate
+                landscapes I have not flown yet all around the world.
             </p>
             <p>
                 For now, I only have a list of regions I compiled from
-                the data I already scraped. Scraping goes on, and new data
-                is being generated regularly, I hope I'll be able to create
-                my proper 3d tile server eventually.
+                the data I already have.
             </p>
+            <div>
+                Roadmap:
+                <ul>
+                  <li>Add more data</li>
+                  <li>Add filters: time of day, day of year</li>
+                  <li>Create a proper 3d tile server for dynamic loading</li>
+                  <li>FPS Mode: Real time navigation to where you are</li>
+                </ul>
+            </div>
             <p>
                 Safe flights!<br/>
                 the dude
